d00c8f8568e0c4f95af69b8581fe30f15d2b177e
Sunil Kumar Vilambi <14075841+svilambi@users.noreply.github.com>
2026-03-16 15:37:29 -0500

    updated

---
+++ js/github.js
@@ -0,0 +1,10 @@
+
+const username="svilambi"
+fetch(`https://api.github.com/users/${username}`)
+.then(r=>r.json())
+.then(d=>{
+document.getElementById("metrics").innerHTML=
+`<h3>Repositories: ${d.public_repos}</h3>
+<h3>Followers: ${d.followers}</h3>
+<h3>Following: ${d.following}</h3>`
+})
